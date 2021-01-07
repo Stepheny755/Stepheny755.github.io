27c41fffd76ef9888ffa87f255fd99c676c61e54
Stephen Yang <stepheny755@gmail.com>
2021-01-06 23:58:54 -0800

    updated index.js for light/dark mode switching

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router,Switch,Route,Link } from "react-router-dom";
 
-import './style/index.css'
+import './style/themes.scss'
+import './style/index.scss'
 
 //import NavBar from './layouts/nav'
 //import { NaviBar } from './layouts/navbar'
 //import Footer from './layouts/footer'
 
+import Header from './contents/header'
 import About from './contents/about'
 import Education from './contents/education'
 import Experience from './contents/experience'
@@ -17,6 +19,30 @@ import Blog from './contents/blog'
 import Notes from './contents/notes'
 
 class Main extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      theme: 0,
+    }
+  }
+
+  toggleTheme(){
+    this.setState({
+      theme:(this.state.theme+1)%2
+    });
+  }
+
+  getTheme(){
+    switch(this.state.theme){
+      case 0:
+        return 'light';
+      case 1:
+        return 'dark';
+      default:
+        return 'light';
+    }
+  }
+
   mainPage(){
     return(
       <React.Fragment>
@@ -26,29 +52,30 @@ class Main extends React.Component{
           <Education/>
           <Experience/>
           <Projects />
-          <Blog />
         </div>
       </React.Fragment>
     );
   }
 
-  /*
-  constructor(props){
-    super(props);
-  }
-  */
   render(){
     return(
     <Router>
+      <Header theme={this.getTheme()} onClick={() => this.toggleTheme()} />
       <Switch>
         <Route path = "/blog">
+          <div className = {`App ${this.getTheme()}`}>
           <Blog />
+          </div>
         </Route>
         <Route path = "/notes">
+          <div className = {`App ${this.getTheme()}`}>
           <Notes />
+          </div>
         </Route>
         <Route path="/">
+          <div className = {`App ${this.getTheme()}`}>
           {this.mainPage()}
+          </div>
         </Route>
       </Switch>
     </Router>
