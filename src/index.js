1a0322a9aabda79c3ccb912474dbbb43454854d6
Stephen <stepheny755@gmail.com>
2021-01-09 21:58:19 -0500

    updated index.js (moved code into routes.js) updated .gitignore updated packages.json added .env

--- src/index.js
+++ src/index.js
@@ -5,18 +5,7 @@ import { BrowserRouter as Router,Switch,Route,Link } from "react-router-dom";
 import './style/themes.scss'
 import './style/index.scss'
 
-//import NavBar from './layouts/nav'
-//import { NaviBar } from './layouts/navbar'
-//import Footer from './layouts/footer'
-
-import Header from './contents/header'
-import About from './contents/about'
-import Education from './contents/education'
-import Experience from './contents/experience'
-import Projects from './contents/projects'
-import Landing from './contents/landing'
-import Blog from './contents/blog'
-import Notes from './contents/notes'
+import Routes from './contents/routes'
 
 class Main extends React.Component{
   constructor(props){
@@ -43,46 +32,12 @@ class Main extends React.Component{
     }
   }
 
-  mainPage(){
-    return(
-      <React.Fragment>
-        <Landing />
-        <div id = "main">
-          <About />
-          <Education/>
-          <Experience/>
-          <Projects />
-        </div>
-      </React.Fragment>
-    );
-  }
-
   render(){
+    console.log(this.getTheme())
     return(
-    <Router>
-      <Header theme={this.getTheme()} onClick={() => this.toggleTheme()} />
-      <Switch>
-        <Route path = "/blog">
-          <div className = {`App ${this.getTheme()}`}>
-          <Blog />
-          </div>
-        </Route>
-        <Route path = "/notes">
-          <div className = {`App ${this.getTheme()}`}>
-          <Notes />
-          </div>
-        </Route>
-        <Route path="/about">
-          <div className = {`App ${this.getTheme()}`}>
-          {this.mainPage()}
-          </div>
-        </Route>
-        <Route path="/">
-          <div className = {`App ${this.getTheme()}`}>
-          </div>
-        </Route>
-      </Switch>
-    </Router>
+    <div>
+    <Routes theme={this.getTheme()} onClick={()=>this.toggleTheme()}/>
+    </div>
   );
   }
 }
